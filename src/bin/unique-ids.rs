ad38156b52cfecf728d4477e9f098d9462a78c49
Esteban Velasquez <estebanvelasquezcardeno@gmail.com>
2024-01-08 21:37:12 -0500

    refac: update node initialization and remove unnecessary code

--- src/bin/unique-ids.rs
+++ src/bin/unique-ids.rs
@@ -1,6 +1,6 @@
 use std::io::{StdoutLock, Write};
 
-use anyhow::{bail, Context};
+use anyhow::Context;
 use serde::{Deserialize, Serialize};
 
 use rustengan::*;
@@ -14,37 +14,28 @@ enum Payload {
         #[serde(rename = "id")]
         guid: String,
     },
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
-    },
-    InitOk,
 }
 
 struct UniqueNode {
+    node: String,
     id: usize,
 }
 
-impl Node<Payload> for UniqueNode {
+impl Node<(), Payload> for UniqueNode {
+    fn from_init(_state: (), init: Init) -> anyhow::Result<Self>
+    where
+        Self: Sized,
+    {
+        Ok(UniqueNode {
+            node: init.node_id,
+            id: 1,
+        })
+    }
+
     fn step(&mut self, input: Message<Payload>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
-            Payload::Init { .. } => {
-                let reply = Message {
-                    src: input.dst,
-                    dst: input.src,
-                    body: Body {
-                        id: Some(self.id),
-                        in_reply_to: input.body.id,
-                        payload: Payload::InitOk,
-                    },
-                };
-                serde_json::to_writer(&mut *output, &reply)
-                    .context("serialize response to init")?;
-                output.write_all(b"\n").context("write trailing newline")?;
-                self.id += 1;
-            }
             Payload::Generate => {
-                let guid = ulid::Ulid::new().to_string();
+                let guid = format!("{}-{}", self.node, self.id);
                 let reply = Message {
                     src: input.dst,
                     dst: input.src,
@@ -59,13 +50,12 @@ impl Node<Payload> for UniqueNode {
                 output.write_all(b"\n").context("write trailing newline")?;
                 self.id += 1;
             }
-            Payload::InitOk { .. } => bail!("received init_ok message"),
-            Payload::GenerateOk { .. } => {},
+            Payload::GenerateOk { .. } => {}
         }
         Ok(())
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    main_loop(UniqueNode { id: 0 })
+    main_loop::<_, UniqueNode, _>(())
 }
